ffc649b3d962c1e0d6c790fd9ae7f9ee17042363
Nishkarsh Raj <500060720@stu.upes.ac.in>
2018-01-24 09:46:13 +0530

    Classes with friend function

---
+++ class.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+class employee
+{
+	int emp_id;
+	string emp_name[20];
+	public:
+	void read(int a,string n)
+	{
+		emp_id=a;
+		emp_name=n;
+	}
+	friend void show(employee E);
+};
+void show(employee E)
+{
+	cout<<"Name of the employee is "<<emp_name<<endl<<"ID of the employee is "<<emp_id;
+}
+int main()
+{
+	employee E;
+	E.read(23402,"Nishkarsh Raj Khare");
+	show(E);
+	return 0;
+}
